69f6d1dbd1e89b28e10aeb8f330b26b8592a29be
Tommy van der Vorst <tommy@pixelspark.nl>
2008-10-06 20:46:10 +0000

    TJShow: - Made cue handling a little more stable. The CueThread can potentially process many cues per 'tick' (if the tick is late, it will just find all cues between the previous tick time and the current and execute them). When the first cue triggered another cue in the same timeline or stopped it, the other cues would still be executed, which is obviously not desirable. Cue::DoAction will now return false if 'linear cue processing' cannot continue, and CueThread will check for this. So this new behaviour is much more correct.
    TJDMX:
    - Universe-dot-channel addressing now works everywhere
    
    git-svn-id: https://svn.pixelspark.nl/svn/corespark/trunk@396 c9a305e2-9f8a-4be3-8c95-b0e5bab41dc9

--- TJShared/include/internal/tjpch.h
+++ TJShared/include/internal/tjpch.h
@@ -1,36 +1,38 @@
 #ifndef _TJSHAREDPCH_H
 #define _TJSHAREDPCH_H
 
-#define _WIN32_WINNT 0x0700
-#define _WIN32_IE 0x0700
-#define _CRT_SECURE_CPP_OVERLOAD_SECURE_NAMES 1
-#define _CRT_RAND_S 1
-#define WIN32_LEAN_AND_MEAN 1
+#ifdef _WIN32
+	#define _WIN32_WINNT 0x0700
+	#define _WIN32_IE 0x0700
+	#define _CRT_SECURE_CPP_OVERLOAD_SECURE_NAMES 1
+	#define _CRT_RAND_S 1
+	#define WIN32_LEAN_AND_MEAN 1
 
-#include <winsock2.h>
-#include <windows.h>
-#include <winspool.h>
-#include <ole2.h>
-#include <ws2tcpip.h>
-#include <Gdiplus.h>
+	#include <winsock2.h>
+	#include <windows.h>
+	#include <winspool.h>
+	#include <ole2.h>
+	#include <ws2tcpip.h>
+	#include <Gdiplus.h>
 
-namespace tj {
-	namespace shared {
-		namespace graphics = ::Gdiplus;
+	namespace tj {
+		namespace shared {
+			namespace graphics = ::Gdiplus;
+		}
 	}
-}
 
-#include <map>
-#include <string>
-#include <sstream>
-#include <algorithm>
-#include <vector>
-#include <set>
-#include <list>
-#include <iomanip>
-#include <fstream>
-#include <deque>
-#include <math.h>
+	#include <map>
+	#include <string>
+	#include <sstream>
+	#include <algorithm>
+	#include <vector>
+	#include <set>
+	#include <list>
+	#include <iomanip>
+	#include <fstream>
+	#include <deque>
+	#include <math.h>
+#endif
 
 // TODO: move tinyxml to other folder
 #include "../../Libraries/tinyxml.h"
